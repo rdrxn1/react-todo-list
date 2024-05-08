836a90c1e87c4fd54b9e3ba145a1129a280ff5a3
Waleed Ibrahim <redraxon@gmail.com>
2024-05-08 06:24:16 +0500

    Fixed text overflowing out of div

--- app/components/TodoList.jsx
+++ app/components/TodoList.jsx
@@ -7,11 +7,12 @@ export default function TodoList(props) {
     const { todos } = props
 
     return (
+        <div className='mx-3.5'>
         <ul className='flex flex-col gap-4 max-w-screen-md w-full mx-auto'>
             {todos.map((todo, todoIndex) => {
                 return(
                     <TodoCard {...props} key={todoIndex} index={todoIndex}>
-                        <p>
+                        <p className='flex-1 truncate'>
                             {todo}
                         </p>
                     </TodoCard>
@@ -19,6 +20,7 @@ export default function TodoList(props) {
 
             })}
         </ul>
+        </div>
     )
 
 }
