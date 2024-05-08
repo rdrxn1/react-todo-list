0f66e58d2c80ec018edb1f0aef06a28230aaca9b
Waleed Ibrahim <redraxon@gmail.com>
2024-05-08 06:25:12 +0500

    Added a toast on text copy

--- app/components/TodoCard.jsx
+++ app/components/TodoCard.jsx
@@ -2,31 +2,44 @@ import { useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPenToSquare, faTrashCan } from '@fortawesome/free-regular-svg-icons'
 import { FaRegCopy } from "react-icons/fa";
+import { toast } from 'sonner';
+
+
 
 export default function TodoCard(props) {
+
+
+
+  
+
+
   const {children, handleDeleteTodos, index, handleEditTodos, handleCopyTodos} = props
   return (
-    <li className='todoItem bg-bkgcolorlight dark:bg-bkgcolor text-txtcolorlight dark:text-txtcolor'>
+    <li className='flex items-center gap-4.5 p-3.5 rounded-[20px]  bg-bkgcolorlight dark:bg-bkgcolor text-txtcolorlight dark:text-txtcolor'>
         {children}
-        <div 
-        className = 'actionsContainer bg-bkgcolorlight dark:bg-bkgcolor'>
+        <div
+        className = 'flex items-center gap-3.5 bg-bkgcolorlight dark:bg-bkgcolor'>
           <button
             onClick={() => {
-              handleCopyTodos(index)
-            }}>
-            <FaRegCopy className='bg-transparent drop-shadow'/>
+              handleCopyTodos(index);
+              toast.success('Text copied to clipboard!', {
+                position: 'bottom-right',
+                duration: 3000,
+              });
+          }}>
+            <FaRegCopy className='bg-transparent drop-shadow transition-opacity duration-200 hover:opacity-70'/>
           </button>
         <button
         onClick={() => {
           handleEditTodos(index)
         }}>
-        <FontAwesomeIcon icon={faPenToSquare} className='bg-transparent drop-shadow'/>
+        <FontAwesomeIcon icon={faPenToSquare} className='bg-transparent drop-shadow transition-opacity duration-200 hover:opacity-70'/>
         </button>
         <button
         onClick={() => {
             handleDeleteTodos(index) 
         }}>
-        <FontAwesomeIcon icon={faTrashCan} className='bg-transparent drop-shadow'/>
+        <FontAwesomeIcon icon={faTrashCan} className='bg-transparent drop-shadow transition-opacity duration-200 hover:opacity-70 focus:opacity-0'/>
         </button>
         </div>
     </li>
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,6 @@
-"use client"
-import { useState, useRef } from "react";
-import SVGButton from "./components/DarkMode.jsx";
+"use client";
+import { useState, useRef, useEffect } from "react";
+import { Toaster } from 'sonner'; 
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import { inter } from '@/app/ui/fonts';
@@ -8,12 +8,59 @@ import "./index.css";
 import { ThemeProvider } from 'next-themes'
 config.autoAddCss = false;
 
-export default function RootLayout({children }) {
+export default function RootLayout({ children }) {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    setIsDarkMode(document.documentElement.classList.contains('dark'));
+    const htmlElement = document.documentElement;
+    const mutationObserver = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        if (mutation.attributeName === 'class') {
+          setIsDarkMode(htmlElement.classList.contains('dark'));
+        }
+      });
+    });
+
+    mutationObserver.observe(htmlElement, { attributes: true });
+
+    // Cleanup function for the observer
+    return () => mutationObserver.disconnect();
+  }, []);
 
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className} bg-mainbkgcolorlight dark:bg-mainbkgcolor`}>
-      <ThemeProvider attribute="class" defaultTheme="system">{children}</ThemeProvider>
+       {isDarkMode ? ( 
+         <Toaster
+          toastOptions={{
+            style: {
+              background: '#131720',
+              color: 'white',
+              border: '0px',
+              borderRadius: '24px',
+              width: '209px',
+            },
+            className: 'class',
+          }}
+        />
+       ) : (
+         <Toaster
+          toastOptions={{
+            style: {
+              background: '#D6E4FF', 
+              color: 'black', 
+              border: '0px',
+              borderRadius: '24px',
+              width: '209px',
+            },
+            className: 'class',
+          }}
+        />
+       )}
+       <ThemeProvider attribute="class" defaultTheme="system">
+         {children}
+       </ThemeProvider>
       </body>
     </html>
   );
